b48738a04a5fc2e9630ee9b1440bd0a8aef826b7
Ayush Raj <rayushkgp@gmail.com>
2025-08-12 21:02:48 +0530

    updated

---
+++ src/main/java/com/conc/analysis/service/MachineInfo.java
@@ -0,0 +1,27 @@
+package com.conc.analysis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.conc.analysis.results.Info;
+
+@Service
+public class MachineInfo {
+
+    @Autowired
+    private Info info;
+
+    public Info getInfo() {
+        String os = System.getProperty("os.name");
+        String version = System.getProperty("os.version");
+        String architecture = System.getProperty("os.arch");
+        String javaVersion = System.getProperty("java.version");
+        String user = System.getProperty("user.name");
+        info.setOs(os);
+        info.setVersion(version);
+        info.setArchitecture(architecture);
+        info.setJavaVersion(javaVersion);
+        info.setUser(user);
+        return info;
+    }
+}
